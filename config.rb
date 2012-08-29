979855e8ca1e5c97c0ad76a73cde8483cf5edd78
Guillermo Winkler <gwinkler@inconcertcc.com>
2012-08-29 00:37:04 -0300

    blog created

--- config.rb
+++ config.rb
@@ -2,10 +2,10 @@
 project_type = :stand_alone
 
 # Publishing paths
-http_path = "/"
-http_images_path = "/images"
-http_fonts_path = "/fonts"
-css_dir = "public/stylesheets"
+http_path = "/guilespi/"
+http_images_path = "/guilespi/images"
+http_fonts_path = "/guilespi/fonts"
+css_dir = "public/guilespi/stylesheets"
 
 # Local development paths
 sass_dir = "sass"
